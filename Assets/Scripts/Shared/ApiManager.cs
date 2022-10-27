0c16bea3b85a0a35f17a9d631176f789b7089874
Marios Issa <Marios.g.issa@gmail.com>
2022-10-27 18:29:08 +0200

    Aggiunti file ignorati alla .sol

---
+++ Assets/Scripts/Shared/ApiManager.cs
@@ -0,0 +1,31 @@
+﻿using DilmerGames.Core.Singletons;
+using Unity.Netcode;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsabile of the synchroniziation of API messages between the server and the clients
+/// </summary>
+
+[RequireComponent(typeof(NetworkObject))] 
+public class ApiManager: NetworkSingleton<ApiManager>
+{ 
+    private static NetworkVariable<NetworkString> ApiMessage = new NetworkVariable<NetworkString>("");
+    public static string ApiMessageString
+    { 
+        get
+        {
+            return ApiMessage.Value;
+        }
+        set
+        {
+            ApiMessage.Value = value;
+        }
+    }
+
+    //private void Start()
+    //{
+
+    //}
+
+}
+
---
+++ Assets/Scripts/Shared/Weather.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+[Serializable]
+public class Weather
+{
+    public int id;
+    public string main;
+}
+
+[Serializable]
+public class WeatherInfo
+{
+    public int id;
+    public string name;
+    public List<Weather> weather;
+}
